8b89d0ea1e0c022286109e61894d2d0cb186d0bf
avilac26 <emanuelavilac@gmail.com>
2025-12-29 22:57:30 -0600

    Created create view inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Index from './pages';
+import Create from './pages/Create';
+import View from './pages/View';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Index />}/>
+          <Route path='/create' element={ <Create />}/>
+          <Route path='/view/:bookId' element={ <View />}/>
+        </Routes>
+      </BrowserRouter>
       
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/Create.jsx
@@ -0,0 +1,126 @@
+import { useState } from "react";
+
+function Create(){
+
+    const[title, SetTitle]=useState("");
+    const[author, SetAuthor]=useState("");
+    const[cover, SetCover]=useState("");
+    const[intro, SetIntro]=useState("");
+    const[completed, SetCompleted]=useState(false);
+    const[review, SetReview]=useState("");
+
+    function handleChange(e){
+        const name = e.target.name;
+        const value = e.target.value;
+        switch (name){
+            case 'title':
+                SetTitle(value);
+                break;
+            case 'author':
+                SetAuthor(value);
+                break;
+            case 'intro':
+                SetIntro(value);
+                break;
+            case 'completed':
+                SetCompleted(e.target.checked);
+                break;
+            case 'review':
+                SetReview(value);
+                break;
+        }
+    }
+
+    function handleOnChangeFile(e){
+        const element = e.target;
+        const file = element.files[0];
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onloadend = function(){
+            SetCover(reader.result.toString());
+        }
+
+    }
+    
+    function handleSubmit(e){
+        e.preventDefault();
+
+        const newBook = {
+            id: crypto.randomUUID(),
+            title,
+            author,
+            cover,
+            intro,
+            completed,
+            review,
+        };
+
+        //TODO: mandar a registrar
+    }
+    
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <h2>Title</h2>
+                    <input 
+                        type="text" 
+                        name="title" 
+                        onChange={handleChange} 
+                        value={title} 
+                    />
+                </div>
+                <div>
+                    <h2>Author</h2>
+                    <input 
+                        type="text" 
+                        name="author" 
+                        onChange={handleChange} 
+                        value={author} 
+                    />
+                </div>
+                <div>
+                    <h2>Cover</h2>
+                    <input 
+                        type="file" 
+                        name="cover" 
+                        onChange={ handleOnChangeFile }
+                    />
+                    <div> {!!cover? <img src={cover} width="200" alt="preview" /> : "" } </div>
+                </div>
+                <div>
+                    <h2>Introduction</h2>
+                    <input 
+                        type="text" 
+                        name="intro" 
+                        onChange={handleChange} 
+                        value={intro} 
+                    />
+                </div>
+                <div>
+                    <h2>Completed</h2>
+                    <input 
+                        type="checkbox" 
+                        name="completed" 
+                        onChange={handleChange} 
+                        value={completed} 
+                    />
+                </div>
+                <div>
+                    <h2>Review</h2>
+                    <input 
+                        type="text" 
+                        name="review" 
+                        onChange={handleChange} 
+                        value={review} 
+                    />
+                </div>
+                <input type="submit" value={"Register books"} />
+            </form>
+        </div>
+    )
+}
+
+export default Create;
